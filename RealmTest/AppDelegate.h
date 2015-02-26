34d4ae75e9c040c66f0202d0d662170ca5b6dc4c
nicolaiduus <dmu-24@DMU-24s-Mac-mini.local>
2015-02-26 10:53:21 +0100

    Initial Commit

---
+++ RealmTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RealmTest
+//
+//  Created by DMU-24 on 26/02/15.
+//  Copyright (c) 2015 DMU-24. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RealmTest/FirstViewController.m
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.m
+//  RealmTest
+//
+//  Created by DMU-24 on 26/02/15.
+//  Copyright (c) 2015 DMU-24. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
